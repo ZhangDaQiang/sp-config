df870421875a20c4938626e91c468a2190021c1f
Administrator <Administrator@DESKTOP-86HLDQK>
2020-01-03 10:36:13 +0800

    1

---
+++ _test/src/_test/Test1.java
@@ -0,0 +1,16 @@
+package _test;
+
+/**
+*@Author:	芹菜
+*@CreationTime:	2020年1月3日 上午10:30:51
+*@Description:
+*/
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
